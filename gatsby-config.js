91c33864d5a325302056c791267af45cbaf68fec
Jerel Miller <jerelmiller@gmail.com>
2021-03-07 22:33:05 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  siteMetadata: {
+    title: 'jerelmiller.com',
+  },
+  plugins: [
+    'gatsby-plugin-gatsby-cloud',
+    'gatsby-plugin-sitemap',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        icon: 'src/images/icon.png',
+      },
+    },
+    'gatsby-plugin-mdx',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: './src/pages/',
+      },
+      __key: 'pages',
+    },
+  ],
+}
